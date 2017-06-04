7bc267823e97b53c6ac8ce3c5c4a4a6efb1a699c
Abiel Gutierrez <abielgutierrez95@gmail.com>
2017-06-04 16:11:25 -0700

    changes to main.py

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ class Config(object):
 		self.num_epochs = 10
 		self.max_seq_len = 22
 		self.batch_size = 32
-		self.MAX_WIDTH = 62
-		self.MAX_HEIGHT = 62
+		self.MAX_WIDTH = 90
+		self.MAX_HEIGHT = 90
 
 class LipReader(object):
 	def __init__(self, config):
@@ -88,7 +88,7 @@ class LipReader(object):
 		plt.ylabel('accuracy')
 		plt.xlabel('epoch')
 		plt.legend(['train', 'test'], loc='upper left')
-		plt.show()
+		plt.savefig('acc_plot.png')
 		# summarize history for loss
 		plt.plot(history.history['loss'])
 		plt.plot(history.history['val_loss'])
@@ -96,7 +96,7 @@ class LipReader(object):
 		plt.ylabel('loss')
 		plt.xlabel('epoch')
 		plt.legend(['train', 'test'], loc='upper left')
-		plt.show()
+		plt.savefig('loss_plot.png')
 
 
 	def load_data(self):
@@ -118,14 +118,14 @@ class LipReader(object):
 
 		else:
 
-			#people = ['F01','F02','F04','F05','F06','F07','F08','F09','F10','F11','M01','M02','M04','M07','M08']
-			people = ['F01','F02','F04','F05', 'F06']
+			people = ['F01','F02','F04','F05','F06','F07','F08','F09','F10','F11','M01','M02','M04','M07','M08']
+			#people = ['F01','F02','F04','F05', 'F06']
 			
 			#removed 'phrases' temporarily from data types
 			data_types = ['words']
 			
-			#folder_enum = ['01','02','03','04','05','06','07','08','09','10']
-			folder_enum = ['01','02','03','04', '05']
+			folder_enum = ['01','02','03','04','05','06','07','08','09','10']
+			#folder_enum = ['01','02','03','04', '05']
 
 			VALIDATION_SPLIT = ['F05']
 			TEST_SPLIT = ['F06']
@@ -139,7 +139,7 @@ class LipReader(object):
 			self.X_test = []
 			self.y_test = [] 
 
-			directory = '../cropped'
+			directory = '../../cropped'
 			for person_id in people:
 				for word_index, word in enumerate(folder_enum):
 					for iteration in folder_enum:
