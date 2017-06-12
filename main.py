984b5269e3108cf5d01ccb11b56d723ab55f7b2a
Abiel Gutierrez <abielgutierrez95@gmail.com>
2017-06-12 16:12:14 -0700

    added confusion

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ from keras.layers.wrappers import TimeDistributed
 from keras.applications.vgg16 import VGG16
 from keras.models import Model
 from keras.preprocessing.image import ImageDataGenerator
+from sklearn.metrics import confusion_matrix
+import itertools
+
 
 class Config(object):
 	def __init__(self, nc, ne, msl, bs, lr, dp):
@@ -23,8 +26,8 @@ class Config(object):
 		self.learning_rate = lr
 		self.MAX_WIDTH = 90
 		self.MAX_HEIGHT = 90
-                self.dropout = dp
-  		np.random.seed(1337)
+				self.dropout = dp
+		np.random.seed(1337)
 
 class LipReader(object):
 	def __init__(self, config):
@@ -137,7 +140,12 @@ class LipReader(object):
 
 		history = model.fit(train_data, one_hot_labels_train, epochs=self.config.num_epochs, batch_size=self.config.batch_size,\
 							validation_data=(val_data, one_hot_labels_val))
-		
+
+		predictions = model.predict(self.X_test)
+
+		labels = ['Begin', 'Choose', 'Connection', 'Navigation', 'Next', 'Previous', 'Start', 'Stop', 'Hello', 'Web']
+		cm = confusion_matrix(self.y_test, predictions, labels=labels)
+		plot_confusion_matrix(cm, labels,normalize=False,title='Confusion matrix', cmap=plt.cm.Blues):
 
 		'''
 		history = model.fit_generator(self.training_generator(), steps_per_epoch=np.shape(self.X_train)[0] / self.config.batch_size,\
@@ -192,6 +200,51 @@ class LipReader(object):
 		plt.xlabel('epoch')
 		plt.legend(['train', 'validation'], loc='upper left')
 		plt.savefig('plots/loss_plot.png')
+
+	def plot_confusion_matrix(cm, classes,normalize=False,title='Confusion matrix', cmap=plt.cm.Blues):
+	"""
+	This function prints and plots the confusion matrix.
+	Normalization can be applied by setting `normalize=True`.
+	"""
+		plt.imshow(cm, interpolation='nearest', cmap=cmap)
+		plt.title(title)
+		plt.colorbar()
+		tick_marks = np.arange(len(classes))
+		plt.xticks(tick_marks, classes, rotation=45)
+		plt.yticks(tick_marks, classes)
+
+		if normalize:
+			cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+			print("Normalized confusion matrix")
+		else:
+			print('Confusion matrix, without normalization')
+
+		thresh = cm.max() / 2.
+		for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+			plt.text(j, i, cm[i, j],
+					 horizontalalignment="center",
+					 color="white" if cm[i, j] > thresh else "black")
+
+		plt.tight_layout()
+		plt.ylabel('True label')
+		plt.xlabel('Predicted label')
+		plt.savefig('confusion.png')
+
+# Compute confusion matrix
+cnf_matrix = confusion_matrix(y_test, y_pred)
+np.set_printoptions(precision=2)
+
+# Plot non-normalized confusion matrix
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=class_names,
+					  title='Confusion matrix, without normalization')
+
+# Plot normalized confusion matrix
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=class_names, normalize=True,
+					  title='Normalized confusion matrix')
+
+plt.show()
 
 	def load_data(self, seen_validation):
 		data_dir = 'data'
